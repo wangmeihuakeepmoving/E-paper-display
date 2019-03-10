43fd3249e99b865b920fa24c98061f42b9c87a01
王玫骅 <31558628+wangmeihuakeepmoving@users.noreply.github.com>
2019-03-10 13:07:11 +0800

    L03 by YushenHu

--- main.c
+++ main.c
@@ -1,22 +1,53 @@
-
 /**
   ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
+  * File Name          : main.c
+  * Description        : Main program body
   ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
   ******************************************************************************
   */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "stm32f1xx_hal.h"
+#include "stm32l0xx_hal.h"
+#include "adc.h"
 #include "spi.h"
 #include "usart.h"
 #include "gpio.h"
 
 /* USER CODE BEGIN Includes */
-#include "EPD_1in54.h"
-#include "DEV_Config.h"
-#include "GUI_Paint.h"
+#include "epd1in54.h"
+#include "epdif.h"
+#include "epdpaint.h"
 #include "imagedata.h"
 #include <stdlib.h>
 /* USER CODE END Includes */
@@ -25,7 +56,8 @@
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
-
+#define COLORED      0
+#define UNCOLORED    1
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -40,15 +72,12 @@ void SystemClock_Config(void);
 
 /* USER CODE END 0 */
 
-/**
-  * @brief  The application entry point.
-  *
-  * @retval None
-  */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
 
+  /* USER CODE BEGIN 1 */
+  unsigned char* frame_buffer = (unsigned char*)malloc(EPD_WIDTH * EPD_HEIGHT / 8);
+	char time_string[] = {'0', '0', ':', '0', '0', '\0'};
   /* USER CODE END 1 */
 
   /* MCU Configuration----------------------------------------------------------*/
@@ -69,143 +98,112 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
   MX_SPI1_Init();
-  /* USER CODE BEGIN 2 */	
-	
-	if(EPD_Init(lut_full_update) != 0) {
-        printf("e-Paper init failed\r\n");
-    }
-    EPD_Clear();
-    DEV_Delay_ms(500);
+  MX_ADC_Init();
 
-    //Create a new image cache
-    UBYTE *BlackImage;		
-		/* you have to edit the startup_stm32fxxx.s file and set a big enough heap size */
-    UWORD Imagesize = ((EPD_WIDTH % 8 == 0)? (EPD_WIDTH / 8 ): (EPD_WIDTH / 8 + 1)) * EPD_HEIGHT;
-    if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
-        printf("Failed to apply for black memory...\r\n");
-				return 0;
-    }
-    printf("Paint_NewImage\r\n");
-		
-    Paint_NewImage(BlackImage, EPD_WIDTH, EPD_HEIGHT, 270, WHITE);
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(WHITE);
-		#if 1   //show image for array    
-    printf("show image for array\r\n");
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(WHITE);
-    Paint_DrawBitMap(gImage_1in54);
-    
-    EPD_Display(BlackImage);
-    DEV_Delay_ms(2000);
-#endif
+  /* USER CODE BEGIN 2 */
+	EPD epd;
+  if (EPD_Init(&epd, lut_full_update) != 0) {
+    printf("e-Paper init failed\n");
+    return -1;
+  }
+  
+  Paint paint;
+  Paint_Init(&paint, frame_buffer, epd.width, epd.height);
+  Paint_Clear(&paint, UNCOLORED);
 
-#if 1   // Drawing on the image
-    //1.Select Image
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(WHITE);
+  /* For simplicity, the arguments are explicit numerical coordinates */
+  /* Write strings to the buffer */
+  Paint_DrawFilledRectangle(&paint, 0, 6, 200, 26, COLORED);
+  Paint_DrawStringAt(&paint, 28, 10, "Hello world!", &Font16, UNCOLORED);
+  Paint_DrawStringAt(&paint, 30, 30, "e-Paper Demo", &Font16, COLORED);
 
-    // 2.Drawing on the image
-    Paint_DrawPoint(5, 10, BLACK, DOT_PIXEL_1X1, DOT_STYLE_DFT);
-    Paint_DrawPoint(5, 25, BLACK, DOT_PIXEL_2X2, DOT_STYLE_DFT);
-    Paint_DrawPoint(5, 40, BLACK, DOT_PIXEL_3X3, DOT_STYLE_DFT);
-    Paint_DrawPoint(5, 55, BLACK, DOT_PIXEL_4X4, DOT_STYLE_DFT);
+  /* Draw something to the frame buffer */
+  Paint_DrawRectangle(&paint, 10, 60, 50, 110, COLORED);
+  Paint_DrawLine(&paint, 10, 60, 50, 110, COLORED);
+  Paint_DrawLine(&paint, 50, 60, 10, 110, COLORED);
+  Paint_DrawCircle(&paint, 120, 80, 30, COLORED);
+  Paint_DrawFilledRectangle(&paint, 10, 130, 50, 180, COLORED);
+  Paint_DrawFilledCircle(&paint, 120, 150, 30, COLORED);
+  
+  /* Display the frame_buffer */
+  EPD_SetFrameMemory(&epd, frame_buffer, 0, 0, Paint_GetWidth(&paint), Paint_GetHeight(&paint));
+  EPD_DisplayFrame(&epd);
+  EPD_DelayMs(&epd, 2000);
 
-    Paint_DrawLine(20, 10, 70, 60, BLACK, LINE_STYLE_SOLID, DOT_PIXEL_1X1);
-    Paint_DrawLine(70, 10, 20, 60, BLACK, LINE_STYLE_SOLID, DOT_PIXEL_1X1);
-    Paint_DrawLine(170, 15, 170, 55, BLACK, LINE_STYLE_DOTTED, DOT_PIXEL_1X1);
-    Paint_DrawLine(150, 35, 190, 35, BLACK, LINE_STYLE_DOTTED, DOT_PIXEL_1X1);
-
-    Paint_DrawRectangle(20, 10, 70, 60, BLACK, DRAW_FILL_EMPTY, DOT_PIXEL_1X1);
-    Paint_DrawRectangle(85, 10, 130, 60, BLACK, DRAW_FILL_FULL, DOT_PIXEL_1X1);
-
-    Paint_DrawCircle(170, 35, 20, BLACK, DRAW_FILL_EMPTY, DOT_PIXEL_1X1);
-    Paint_DrawCircle(170, 85, 20, BLACK, DRAW_FILL_FULL, DOT_PIXEL_1X1);
-    Paint_DrawString_EN(5, 85, "waveshare", &Font20, BLACK, WHITE);
-    Paint_DrawNum(5, 110, 123456789, &Font20, BLACK, WHITE);
-
-    Paint_DrawString_CN(5, 135,"ÄãºÃabc", &Font12CN, BLACK, WHITE);
-    Paint_DrawString_CN(5, 160, "Î¢Ñ©µç×Ó", &Font24CN, WHITE, BLACK);
-
-    EPD_Display(BlackImage);
-    DEV_Delay_ms(2000);
-#endif
-
-#if 1   //Partial refresh, example shows time    
-    if(EPD_Init(lut_partial_update) != 0) {
-        printf("e-Paper init failed\r\n");
-    }
-		Paint_SelectImage(BlackImage);
-    PAINT_TIME sPaint_time;
-    sPaint_time.Hour = 12;
-    sPaint_time.Min = 34;
-    sPaint_time.Sec = 56;
-    for (;;) {
-        sPaint_time.Sec = sPaint_time.Sec + 1;
-        if (sPaint_time.Sec == 60) {
-            sPaint_time.Min = sPaint_time.Min + 1;
-            sPaint_time.Sec = 0;
-            if (sPaint_time.Min == 60) {
-                sPaint_time.Hour =  sPaint_time.Hour + 1;
-                sPaint_time.Min = 0;
-                if (sPaint_time.Hour == 24) {
-                    sPaint_time.Hour = 0;
-                    sPaint_time.Min = 0;
-                    sPaint_time.Sec = 0;
-                }
-            }
-        }
-        Paint_ClearWindows(15, 65, 15 + Font20.Width * 7, 65 + Font20.Height, WHITE);
-        Paint_DrawTime(15, 65, &sPaint_time, &Font20, WHITE, BLACK);
-
-				EPD_Display(BlackImage);
-        DEV_Delay_ms(500);//Analog clock 1s
-    }
-
-#endif
-
-    printf("Goto Sleep mode...\r\n");
-    EPD_Sleep();
-    free(BlackImage);
-    BlackImage = NULL;
-		
+  if (EPD_Init(&epd, lut_partial_update) != 0) {
+    printf("e-Paper init failed\n");
+    return -1;
+  }
+  /**
+    *  there are 2 memory areas embedded in the e-paper display
+    *  and once the display is refreshed, the memory area will be auto-toggled,
+    *  i.e. the next action of SetFrameMemory will set the other memory area
+    *  therefore you have to set the frame memory and refresh the display twice.
+    */
+  EPD_SetFrameMemory(&epd, MAIN_DATA, 0, 0, epd.width, epd.height);
+  EPD_DisplayFrame(&epd);
+  EPD_SetFrameMemory(&epd, MAIN_DATA, 0, 0, epd.width, epd.height);
+  EPD_DisplayFrame(&epd);
+  
+	EPD_SetFrameMemory(&epd, MAIN_DATA, 150, 150, epd.width, epd.height);
+  EPD_DisplayFrame(&epd);
+  EPD_SetFrameMemory(&epd, MAIN_DATA, 150, 150, epd.width, epd.height);
+  EPD_DisplayFrame(&epd);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
+    /*time_string[0] = time_now_s / 60 / 10 + '0';
+    time_string[1] = time_now_s / 60 % 10 + '0';
+    time_string[3] = time_now_s % 60 / 10 + '0';
+    time_string[4] = time_now_s % 60 % 10 + '0';
+    */
+		time_string[0] = '1';
+    time_string[1] = '2';
+    time_string[3] = '3';
+    time_string[4] = '4';
+    Paint_SetWidth(&paint, 32);
+    Paint_SetHeight(&paint, 96);
+    Paint_SetRotate(&paint, ROTATE_270);
 
+    Paint_Clear(&paint, UNCOLORED);
+    Paint_DrawStringAt(&paint, 0, 4, time_string, &Font24, COLORED);
+    EPD_SetFrameMemory(&epd, frame_buffer, 80, 72, Paint_GetWidth(&paint), Paint_GetHeight(&paint));
+    EPD_DisplayFrame(&epd);
+
+    EPD_DelayMs(&epd, 500);
   }
   /* USER CODE END 3 */
 
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+/** System Clock Configuration
+*/
 void SystemClock_Config(void)
 {
 
   RCC_OscInitTypeDef RCC_OscInitStruct;
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_PeriphCLKInitTypeDef PeriphClkInit;
+
+    /**Configure the main internal regulator output voltage 
+    */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
     /**Initializes the CPU, AHB and APB busses clocks 
     */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_5;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
@@ -215,12 +213,19 @@ void SystemClock_Config(void)
     */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
   }
@@ -243,43 +248,45 @@ void SystemClock_Config(void)
 
 /**
   * @brief  This function is executed in case of error occurrence.
-  * @param  file: The file name as string.
-  * @param  line: The line in file as a number.
+  * @param  None
   * @retval None
   */
-void _Error_Handler(char *file, int line)
+void _Error_Handler(char * file, int line)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-  while(1)
+  while(1) 
   {
   }
-  /* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */ 
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
+
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
 void assert_failed(uint8_t* file, uint32_t line)
-{ 
+{
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
+
 }
-#endif /* USE_FULL_ASSERT */
+
+#endif
 
 /**
   * @}
-  */
+  */ 
 
 /**
   * @}
-  */
+*/ 
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
