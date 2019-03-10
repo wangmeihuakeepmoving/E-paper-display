b37ffd11b67a1c8e15d6437dd4203fdf7eebffa3
王玫骅 <31558628+wangmeihuakeepmoving@users.noreply.github.com>
2019-03-10 14:22:38 +0800

    compare demo with V1 picture display

--- EPD_1in54.c
+++ EPD_1in54.c
@@ -1,240 +1,271 @@
-/*****************************************************************************
-* | File      	:	EPD_1in54.c
-* | Author      :   Waveshare team
-* | Function    :   Electronic paper driver
-* | Info        :
-*----------------
-* |	This version:   V2.0
-* | Date        :   2018-10-30
-* | Info        :   
-* 1.Remove:ImageBuff[EPD_HEIGHT * EPD_WIDTH / 8]
-* 2.Change:EPD_Display(UBYTE *Image)
-*   Need to pass parameters: pointer to cached data
-* 3.Change:
-*   EPD_RST -> EPD_RST_PIN
-*   EPD_DC -> EPD_DC_PIN
-*   EPD_CS -> EPD_CS_PIN
-*   EPD_BUSY -> EPD_BUSY_PIN
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documnetation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to  whom the Software is
-# furished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-******************************************************************************/
-#include "EPD_1in54.h"
-#include "Debug.h"
+/**
+ *  @filename   :   epd1in54b.c
+ *  @brief      :   Implements for e-paper library
+ *  @author     :   Yehui from Waveshare
+ *
+ *  Copyright (C) Waveshare     September 12 2017
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documnetation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to  whom the Software is
+ * furished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-const unsigned char lut_full_update[] = {
-    0x02, 0x02, 0x01, 0x11, 0x12, 0x12, 0x22, 0x22,
-    0x66, 0x69, 0x69, 0x59, 0x58, 0x99, 0x99, 0x88,
-    0x00, 0x00, 0x00, 0x00, 0xF8, 0xB4, 0x13, 0x51,
+#include <stdlib.h>
+#include "epd1in54.h"
+#include "epdif.h"
+
+int EPD_Init(EPD* epd, const unsigned char* lut) {
+  epd->reset_pin = RST_PIN;
+  epd->dc_pin = DC_PIN;
+  epd->cs_pin = CS_PIN;
+  epd->busy_pin = BUSY_PIN;
+  epd->width = EPD_WIDTH;
+  epd->height = EPD_HEIGHT;
+  /* this calls the peripheral hardware interface, see epdif */
+  if (EpdInitCallback() != 0) {
+    return -1;
+  }
+  epd->lut = lut;
+  /* EPD hardware init start */
+  EPD_Reset(epd);
+  EPD_SendCommand(epd, DRIVER_OUTPUT_CONTROL);
+  EPD_SendData(epd, (EPD_HEIGHT - 1) & 0xFF);
+  EPD_SendData(epd, ((EPD_HEIGHT - 1) >> 8) & 0xFF);
+  EPD_SendData(epd, 0x00);                     // GD = 0; SM = 0; TB = 0;
+  EPD_SendCommand(epd, BOOSTER_SOFT_START_CONTROL);
+  EPD_SendData(epd, 0xD7);
+  EPD_SendData(epd, 0xD6);
+  EPD_SendData(epd, 0x9D);
+  EPD_SendCommand(epd, WRITE_VCOM_REGISTER);
+  EPD_SendData(epd, 0xA8);                     // VCOM 7C
+  EPD_SendCommand(epd, SET_DUMMY_LINE_PERIOD);
+  EPD_SendData(epd, 0x1A);                     // 4 dummy lines per gate
+  EPD_SendCommand(epd, SET_GATE_TIME);
+  EPD_SendData(epd, 0x08);                     // 2us per line
+  EPD_SendCommand(epd, DATA_ENTRY_MODE_SETTING);
+  EPD_SendData(epd, 0x03);                     // X increment; Y increment
+  EPD_SetLut(epd, epd->lut);
+  /* EPD hardware init end */
+  return 0;
+}
+
+/**
+ *  @brief: this calls the corresponding function from epdif.h
+ *          usually there is no need to change this function
+ */
+void EPD_DigitalWrite(EPD* epd, int pin, int value) {
+  EpdDigitalWriteCallback(pin, value);
+}
+
+/**
+ *  @brief: this calls the corresponding function from epdif.h
+ *          usually there is no need to change this function
+ */
+int EPD_DigitalRead(EPD* epd, int pin) {
+  return EpdDigitalReadCallback(pin);
+}
+
+/**
+ *  @brief: this calls the corresponding function from epdif.h
+ *          usually there is no need to change this function
+ */
+void EPD_DelayMs(EPD* epd, unsigned int delaytime) {  // 1ms
+  EpdDelayMsCallback(delaytime);
+}
+
+/**
+ *  @brief: basic function for sending commands
+ */
+void EPD_SendCommand(EPD* epd, unsigned char command) {
+  EPD_DigitalWrite(epd, epd->dc_pin, LOW);
+  EpdSpiTransferCallback(command);
+}
+
+/**
+ *  @brief: basic function for sending data
+ */
+void EPD_SendData(EPD* epd, unsigned char data) {
+  EPD_DigitalWrite(epd, epd->dc_pin, HIGH);
+  EpdSpiTransferCallback(data);
+}
+
+/**
+ *  @brief: Wait until the busy_pin goes LOW
+ */
+void EPD_WaitUntilIdle(EPD* epd) {
+  while(EPD_DigitalRead(epd, epd->busy_pin) == HIGH) {      //0: busy, 1: idle
+    EPD_DelayMs(epd, 100);
+  }      
+}
+
+/**
+ *  @brief: module reset.
+ *          often used to awaken the module in deep sleep,
+ *          see EPD::Sleep();
+ */
+void EPD_Reset(EPD* epd) {
+  EPD_DigitalWrite(epd, epd->reset_pin, LOW);                //module reset    
+  EPD_DelayMs(epd, 200);
+  EPD_DigitalWrite(epd, epd->reset_pin, HIGH);
+  EPD_DelayMs(epd, 200);    
+}
+
+/**
+ *  @brief: put an image buffer to the frame memory.
+ *          this won't update the display.
+ */
+void EPD_SetFrameMemory(
+  EPD* epd,
+  const unsigned char* image_buffer,
+  int x,
+  int y,
+  int image_width,
+  int image_height
+) {
+  int x_end;
+  int y_end;
+
+  if (
+    image_buffer == NULL ||
+    x < 0 || image_width < 0 ||
+    y < 0 || image_height < 0
+  ) {
+    return;
+  }
+  /* x point must be the multiple of 8 or the last 3 bits will be ignored */
+  x &= 0xF8;
+  image_width &= 0xF8;
+  if (x + image_width >= epd->width) {
+    x_end = epd->width - 1;
+  } else {
+    x_end = x + image_width - 1;
+  }
+  if (y + image_height >= epd->height) {
+    y_end = epd->height - 1;
+  } else {
+    y_end = y + image_height - 1;
+  }
+  EPD_SetMemoryArea(epd, x, y, x_end, y_end);
+  EPD_SetMemoryPointer(epd, x, y);
+  EPD_SendCommand(epd, WRITE_RAM);
+  /* send the image data */
+  for (int j = 0; j < y_end - y + 1; j++) {
+    for (int i = 0; i < (x_end - x + 1) / 8; i++) {
+      EPD_SendData(epd, image_buffer[i + j * (image_width / 8)]);
+    }
+  }
+}
+
+/**
+*  @brief: clear the frame memory with the specified color.
+*          this won't update the display.
+*/
+void EPD_ClearFrameMemory(EPD* epd, unsigned char color) {
+  EPD_SetMemoryArea(epd, 0, 0, epd->width - 1, epd->height - 1);
+  EPD_SetMemoryPointer(epd, 0, 0);
+  EPD_SendCommand(epd, WRITE_RAM);
+  /* send the color data */
+  for (int i = 0; i < epd->width / 8 * epd->height; i++) {
+    EPD_SendData(epd, color);
+  }
+}
+
+/**
+*  @brief: update the display
+*          there are 2 memory areas embedded in the e-paper display
+*          but once this function is called,
+*          the the next action of SetFrameMemory or ClearFrame will 
+*          set the other memory area.
+*/
+void EPD_DisplayFrame(EPD* epd) {
+  EPD_SendCommand(epd, DISPLAY_UPDATE_CONTROL_2);
+  EPD_SendData(epd, 0xC4);
+  EPD_SendCommand(epd, MASTER_ACTIVATION);
+  EPD_SendCommand(epd, TERMINATE_FRAME_READ_WRITE);
+  EPD_WaitUntilIdle(epd);
+}
+
+/**
+ *  @brief: After this command is transmitted, the chip would enter the 
+ *          deep-sleep mode to save power. 
+ *          The deep sleep mode would return to standby by hardware reset. 
+ *          You can use EPD_Init() to awaken
+ */
+void EPD_Sleep(EPD* epd) {
+  EPD_SendCommand(epd, DEEP_SLEEP_MODE);
+  EPD_WaitUntilIdle(epd);
+}
+
+/**
+ *  @brief: set the look-up tables
+ */
+static void EPD_SetLut(EPD* epd, const unsigned char* lut) {
+  epd->lut = lut;
+  EPD_SendCommand(epd, WRITE_LUT_REGISTER);
+  /* the length of look-up table is 30 bytes */
+  for (int i = 0; i < 30; i++) {
+    EPD_SendData(epd, epd->lut[i]);
+  } 
+}
+
+/**
+ *  @brief: private function to specify the memory area for data R/W
+ */
+static void EPD_SetMemoryArea(EPD* epd, int x_start, int y_start, int x_end, int y_end) {
+  EPD_SendCommand(epd, SET_RAM_X_ADDRESS_START_END_POSITION);
+  /* x point must be the multiple of 8 or the last 3 bits will be ignored */
+  EPD_SendData(epd, (x_start >> 3) & 0xFF);
+  EPD_SendData(epd, (x_end >> 3) & 0xFF);
+  EPD_SendCommand(epd, SET_RAM_Y_ADDRESS_START_END_POSITION);
+  EPD_SendData(epd, y_start & 0xFF);
+  EPD_SendData(epd, (y_start >> 8) & 0xFF);
+  EPD_SendData(epd, y_end & 0xFF);
+  EPD_SendData(epd, (y_end >> 8) & 0xFF);
+}
+
+/**
+ *  @brief: private function to specify the start point for data R/W
+ */
+static void EPD_SetMemoryPointer(EPD* epd, int x, int y) {
+  EPD_SendCommand(epd, SET_RAM_X_ADDRESS_COUNTER);
+  /* x point must be the multiple of 8 or the last 3 bits will be ignored */
+  EPD_SendData(epd, (x >> 3) & 0xFF);
+  EPD_SendCommand(epd, SET_RAM_Y_ADDRESS_COUNTER);
+  EPD_SendData(epd, y & 0xFF);
+  EPD_SendData(epd, (y >> 8) & 0xFF);
+  EPD_WaitUntilIdle(epd);
+}
+
+const unsigned char lut_full_update[] =
+{
+    0x02, 0x02, 0x01, 0x11, 0x12, 0x12, 0x22, 0x22, 
+    0x66, 0x69, 0x69, 0x59, 0x58, 0x99, 0x99, 0x88, 
+    0x00, 0x00, 0x00, 0x00, 0xF8, 0xB4, 0x13, 0x51, 
     0x35, 0x51, 0x51, 0x19, 0x01, 0x00
 };
 
-const unsigned char lut_partial_update[] = {
-    0x10, 0x18, 0x18, 0x08, 0x18, 0x18, 0x08, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x13, 0x14, 0x44, 0x12,
+const unsigned char lut_partial_update[] =
+{
+    0x10, 0x18, 0x18, 0x08, 0x18, 0x18, 0x08, 0x00, 
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+    0x00, 0x00, 0x00, 0x00, 0x13, 0x14, 0x44, 0x12, 
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
-/******************************************************************************
-function :	Software reset
-parameter:
-******************************************************************************/
-static void EPD_Reset(void)
-{
-    DEV_Digital_Write(EPD_RST_PIN, 1);
-    DEV_Delay_ms(200);
-    DEV_Digital_Write(EPD_RST_PIN, 0);
-    DEV_Delay_ms(200);
-    DEV_Digital_Write(EPD_RST_PIN, 1);
-    DEV_Delay_ms(200);
-}
+/* END OF FILE */
 
-/******************************************************************************
-function :	send command
-parameter:
-     Reg : Command register
-******************************************************************************/
-static void EPD_SendCommand(UBYTE Reg)
-{
-    DEV_Digital_Write(EPD_DC_PIN, 0);
-    DEV_Digital_Write(EPD_CS_PIN, 0);
-    DEV_SPI_WriteByte(Reg);
-    DEV_Digital_Write(EPD_CS_PIN, 1);
-}
 
-/******************************************************************************
-function :	send data
-parameter:
-    Data : Write data
-******************************************************************************/
-static void EPD_SendData(UBYTE Data)
-{
-    DEV_Digital_Write(EPD_DC_PIN, 1);
-    DEV_Digital_Write(EPD_CS_PIN, 0);
-    DEV_SPI_WriteByte(Data);
-    DEV_Digital_Write(EPD_CS_PIN, 1);
-}
-
-/******************************************************************************
-function :	Wait until the busy_pin goes LOW
-parameter:
-******************************************************************************/
-void EPD_WaitUntilIdle(void)
-{
-    Debug("e-Paper busy\r\n");
-    while(DEV_Digital_Read(EPD_BUSY_PIN) == 1) {      //LOW: idle, HIGH: busy
-        DEV_Delay_ms(100);
-    }
-    Debug("e-Paper busy release\r\n");
-}
-
-/******************************************************************************
-function :	Setting the display window
-parameter:
-******************************************************************************/
-static void EPD_SetWindows(UWORD Xstart, UWORD Ystart, UWORD Xend, UWORD Yend)
-{
-    EPD_SendCommand(SET_RAM_X_ADDRESS_START_END_POSITION);
-    EPD_SendData((Xstart >> 3) & 0xFF);
-    EPD_SendData((Xend >> 3) & 0xFF);
-
-    EPD_SendCommand(SET_RAM_Y_ADDRESS_START_END_POSITION);
-    EPD_SendData(Ystart & 0xFF);
-    EPD_SendData((Ystart >> 8) & 0xFF);
-    EPD_SendData(Yend & 0xFF);
-    EPD_SendData((Yend >> 8) & 0xFF);
-}
-
-/******************************************************************************
-function :	Set Cursor
-parameter:
-******************************************************************************/
-static void EPD_SetCursor(UWORD Xstart, UWORD Ystart)
-{
-    EPD_SendCommand(SET_RAM_X_ADDRESS_COUNTER);
-    EPD_SendData((Xstart >> 3) & 0xFF);
-
-    EPD_SendCommand(SET_RAM_Y_ADDRESS_COUNTER);
-    EPD_SendData(Ystart & 0xFF);
-    EPD_SendData((Ystart >> 8) & 0xFF);
-
-}
-
-/******************************************************************************
-function :	Turn On Display
-parameter:
-******************************************************************************/
-static void EPD_TurnOnDisplay(void)
-{
-    EPD_SendCommand(DISPLAY_UPDATE_CONTROL_2);
-    EPD_SendData(0xC4);
-    EPD_SendCommand(MASTER_ACTIVATION);
-    EPD_SendCommand(TERMINATE_FRAME_READ_WRITE);
-
-    EPD_WaitUntilIdle();
-}
-
-/******************************************************************************
-function :	Initialize the e-Paper register
-parameter:
-******************************************************************************/
-UBYTE EPD_Init(const unsigned char* lut)
-{
-    EPD_Reset();
-
-    EPD_SendCommand(DRIVER_OUTPUT_CONTROL);
-    EPD_SendData((EPD_HEIGHT - 1) & 0xFF);
-    EPD_SendData(((EPD_HEIGHT - 1) >> 8) & 0xFF);
-    EPD_SendData(0x00);                     // GD = 0; SM = 0; TB = 0;
-    EPD_SendCommand(BOOSTER_SOFT_START_CONTROL);
-    EPD_SendData(0xD7);
-    EPD_SendData(0xD6);
-    EPD_SendData(0x9D);
-    EPD_SendCommand(WRITE_VCOM_REGISTER);
-    EPD_SendData(0xA8);                     // VCOM 7C
-    EPD_SendCommand(SET_DUMMY_LINE_PERIOD);
-    EPD_SendData(0x1A);                     // 4 dummy lines per gate
-    EPD_SendCommand(SET_GATE_TIME);
-    EPD_SendData(0x08);                     // 2us per line
-    EPD_SendCommand(DATA_ENTRY_MODE_SETTING);
-    EPD_SendData(0x03);
-
-    //set the look-up table register
-    EPD_SendCommand(WRITE_LUT_REGISTER);
-    for (UWORD i = 0; i < 30; i++) {
-        EPD_SendData(lut[i]);
-    }
-    return 0;
-}
-
-/******************************************************************************
-function :	Clear screen
-parameter:
-******************************************************************************/
-void EPD_Clear(void)
-{
-    UWORD Width, Height;
-    Width = (EPD_WIDTH % 8 == 0)? (EPD_WIDTH / 8 ): (EPD_WIDTH / 8 + 1);
-    Height = EPD_HEIGHT;
-    EPD_SetWindows(0, 0, EPD_WIDTH, EPD_HEIGHT);
-    for (UWORD j = 0; j < Height; j++) {
-        EPD_SetCursor(0, j);
-        EPD_SendCommand(WRITE_RAM);
-        for (UWORD i = 0; i < Width; i++) {
-            EPD_SendData(0XFF);
-        }
-    }
-    EPD_TurnOnDisplay();
-}
-
-/******************************************************************************
-function :	Sends the image buffer in RAM to e-Paper and displays
-parameter:
-******************************************************************************/
-void EPD_Display(UBYTE *Image)
-{
-    UWORD Width, Height;
-    Width = (EPD_WIDTH % 8 == 0)? (EPD_WIDTH / 8 ): (EPD_WIDTH / 8 + 1);
-    Height = EPD_HEIGHT;
-
-    UDOUBLE Addr = 0;
-    // UDOUBLE Offset = ImageName;
-    EPD_SetWindows(0, 0, EPD_WIDTH, EPD_HEIGHT);
-    for (UWORD j = 0; j < Height; j++) {
-        EPD_SetCursor(0, j);
-        EPD_SendCommand(WRITE_RAM);
-        for (UWORD i = 0; i < Width; i++) {
-            Addr = i + j * Width;
-            EPD_SendData(Image[Addr]);
-        }
-    }
-    EPD_TurnOnDisplay();
-}
-
-/******************************************************************************
-function :	Enter sleep mode
-parameter:
-******************************************************************************/
-void EPD_Sleep(void)
-{
-    EPD_SendCommand(DEEP_SLEEP_MODE);
-    EPD_SendData(0x01);
-    // EPD_WaitUntilIdle();
-}
